ade99741bb8f048d5ed793ed05cbfb5ab88efd26
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-11-29 19:22:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ def index():
 @app.route('/', methods=['POST'])
 def renderkey():
   password = request.form['text']
+  with open('ips.txt') as f:
+    if request.remote_addr in f.read():
+        return render_template('index.html', token='BRUH SCREW U')
   if (password == os.getenv('password')):
     token = os.getenv('token')
   else:
+    f = open("ips.txt", "a")
+    f.write(request.remote_addr+"\n")
+    f.close()
     token = "BRUH SCREW U"
 
   return render_template('index.html', token=token)
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=4000)
